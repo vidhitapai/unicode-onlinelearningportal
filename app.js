43c077ba4fbd4364f906c2f315d70b1658bfaef3
Vidhita Pai <vidhitapai@gmail.com>
2021-08-28 12:47:28 +0530

    Made some changes

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 require('./connection');
 
-const userRouter = require('./routers/courseRoutes');
+const courseRouter = require('./routers/courseRoutes');
+const userRouter = require('./routers/userRoutes');
 
 // use port 3000 unless there exists a preconfigured port
 const port = process.env.PORT || 3000;
 
 //course routes
-app.use('/courses', userRouter);
-
+app.use('/courses', courseRouter);
+app.use('/users', userRouter);
 
 app.listen(port, () => {
     console.log('Server is active!')
--- connection.js
+++ connection.js
@@ -9,5 +9,5 @@ const connectionParameters = {
 const url = 'mongodb+srv://primehide:maximus@cluster0.tq0vb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 const connection = mongoose.connect(url, connectionParameters)
-    .then(() => console.log("Server is active!!"))
+    .then(() => console.log("Connected to server!"))
     .catch((err) => console.log(err));
