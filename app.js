aff089837757166d0edd5c595666feed162113eb
Vidhita Pai <vidhitapai@gmail.com>
2021-08-31 19:25:51 +0530

    Error handling for incorrect routes

--- app.js
+++ app.js
@@ -12,6 +12,22 @@ const port = process.env.PORT || 3000;
 app.use('/courses', courseRouter);
 app.use('/users', userRouter);
 
+//error handling for incorrect routes
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 app.listen(port, () => {
     console.log('Server is active!')
 });
