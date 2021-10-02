543d83f46e10b096db1ecc6adf22afa0caecef5c
Vidhita Pai <vidhitapai@gmail.com>
2021-10-02 14:53:06 +0530

    Added login route

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -4,11 +4,12 @@ const userController = require('../controllers/user');
 const router = new express.Router();
 
 router.post('/createuser', userController.user_create);
-router.delete('/deleteuser', userController.user_delete);
+router.post('/login', userController.user_login);
+router.delete('/deleteuser/ :name', userController.user_delete);
 router.get('/view', userController.user_view);
 router.get('/view/name/:name', userController.user_viewByName);
 router.get('/view/student/', userController.user_viewByType_student);
 router.get('/view/instructor/', userController.user_viewByType_instructor);
-router.put('updateuser/:email', userController.user_update);
+router.put('/updateuser/:email', userController.user_update);
 
 module.exports = router;
