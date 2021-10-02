7f1ff5ff49a1fb743d02a2f8939e3058b29fe0bb
Vidhita Pai <vidhitapai@gmail.com>
2021-10-02 14:51:26 +0530

    Hashed passwords, generated authentication tokens using middleware

--- models/users.js
+++ models/users.js
@@ -1,5 +1,9 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const validate = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Course = require('../models/courses');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -24,7 +28,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         minLength: 6,
-        maxLength: 15
     },
     enrolledIn: {
         type: [mongoose.Schema.Types.ObjectId],
@@ -34,8 +37,50 @@ const userSchema = new mongoose.Schema({
     coursesCreated: {
         type: [mongoose.Schema.Types.ObjectId],
         ref: 'Course'
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+//generating tokens for authentication, accessible on instances
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, 'justinvisiblethings');
+    
+    //saving token for each user 
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+
+    return token;
+}
+
+//checking the credentials before logging in, accessible on models
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({email: email});
+    if(!user) {
+        throw new Error('Sorry, unable to login!');
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if(!match) {
+        throw new Error('Sorry, unable to login!');
+    }
+
+    return user;
+}
+
+//hashing the plaintext password before saving
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+})
 
 const User = mongoose.model('User', userSchema);
 
