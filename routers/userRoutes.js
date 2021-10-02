36e6519d31b5251f1bae093e8f9be2e0bef5ef8f
Vidhita Pai <vidhitapai@gmail.com>
2021-10-02 16:18:09 +0530

    Updated routes

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,15 +1,18 @@
 const express = require ('express');
 const userController = require('../controllers/user');
+const auth = require('../middleware/auth');
 
 const router = new express.Router();
 
 router.post('/createuser', userController.user_create);
 router.post('/login', userController.user_login);
-router.delete('/deleteuser/ :name', userController.user_delete);
-router.get('/view', userController.user_view);
-router.get('/view/name/:name', userController.user_viewByName);
-router.get('/view/student/', userController.user_viewByType_student);
-router.get('/view/instructor/', userController.user_viewByType_instructor);
-router.put('/updateuser/:email', userController.user_update);
+router.post('/logout', auth, userController.user_logout);
+router.post('/logoutAll', auth, userController.user_logoutOfAll);
+router.delete('/deleteuser/ :name', auth, userController.user_delete);
+router.get('/view/me', auth, userController.user_view);
+router.get('/view/name/:name', auth, userController.user_viewByName);
+router.get('/view/student/', auth, userController.user_viewByType_student);
+router.get('/view/instructor/', auth, userController.user_viewByType_instructor);
+router.put('/updateuser/:email', auth, userController.user_update);
 
 module.exports = router;
