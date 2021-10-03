fc1357026bacca250aa6f614cad195111b51694a
Vidhita Pai <vidhitapai@gmail.com>
2021-10-03 11:35:32 +0530

    Authenticated course endpoints

--- routers/courseRoutes.js
+++ routers/courseRoutes.js
@@ -1,13 +1,15 @@
 const express = require ('express');
 const courseController = require('../controllers/course');
+const auth = require('../middleware/auth');
 
 const router = new express.Router();
 
-router.post('/createcourse', courseController.course_create);
-router.delete('/deletecourse/:name', courseController.course_delete);
+router.post('/createcourse', auth, courseController.course_create);
+router.delete('/deletecourse/:name', auth, courseController.course_delete);
 router.get('/view', courseController.course_view);
-router.get('/name/:name', courseController.course_viewByName);
-router.get('/instructor/:instructor', courseController.course_viewByInstructor);
-router.put('updatecourse/:name', courseController.course_update);
+router.get('/view/self', auth, courseController.course_viewEnrolledIn);
+router.get('/view/:id', courseController.course_viewById);
+//router.get('/instructor/:instructor', courseController.course_viewByInstructor);
+router.put('/updatecourse/:name', courseController.course_update);
 
 module.exports = router;
