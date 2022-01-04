3bd8d6ff629d97529d631e5a3e24b31b0503cd45
vidhitapai <vidhitapai@gmail.com>
2022-01-04 19:25:58 +0530

    Added testing for user routes

---
+++ test/fixtures/db.js
@@ -0,0 +1,41 @@
+// Importing modules
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const User = require('../../models/users.js');
+const Course = require('../../models/courses.js');
+const { course_viewById } = require('../../controllers/course');
+const dotenv = require('dotenv').config();
+
+// Creating dummy database
+const userID = new mongoose.Types.ObjectId();
+const courseID = new mongoose.Types.ObjectId();
+
+const user = {
+    _id: userID,
+    name: "John Doe",
+    email: "johndoe@gmail.com",
+    password: "iamjohndoe",
+    userType: "INSTRUCTOR",
+    tokens: [{token: jwt.sign({ _id: userID.toString() }, process.env.JWT_KEY)}]
+};
+
+const course = {
+    _id: courseID,
+    name: "Learn Web-Dev",
+    instructor: userID,
+    courseType: "Web Development",
+    rating: "4"
+};
+
+const dbReq = async () => {
+    await User.deleteMany();
+    await Course.deleteMany();
+    await new User(user).save();
+    await new Course(course).save();
+};
+
+module.exports = {
+    user,
+    course,
+    dbReq
+};
---
+++ test/testUser.js
@@ -0,0 +1,120 @@
+// Importing modules
+const chai = require('chai');
+const chaihttp = require('chai-http');
+const { user, course, dbReq } = require('./fixtures/db.js');
+const app = require('../app.js');
+
+// Requiring should style of chai js
+const should = require('chai').should();
+
+chai.use(chaihttp);
+
+beforeEach(dbReq);
+
+describe('Create User', () => {
+    it('should create a new user', (done) => {
+      let userOne = {
+        name: 'Julie Doe',
+        email: 'doejulie@gmail.com',
+        password: 'iamdoejulie',
+        userType: 'INSTRUCTOR',
+      };
+      chai
+        .request(app)
+        .post('/users/createuser')
+        .send(userOne)
+        .end((err, res) => {
+            res.should.have.status(201);
+            res.body.should.be.a('object');
+            res.body.should.have.property('message');
+            res.body.should.have.property('data');
+            done();
+         });
+    });
+});
+
+
+describe('User Login', () => {
+    it('should log the user in', (done) => {
+      chai
+        .request(app)
+        .post('/users/login')
+        .send({
+          email: user.email,
+          password: user.password,
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+  });
+
+describe('User Logout', () => {
+    it('should log the user out', (done) => {
+      let utokens = user.tokens;
+      chai
+        .request(app)
+        .post('/users/logout')
+        .set('Authorization', `Bearer ${utokens[0].token}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message');
+          done();
+        });
+    });
+  });
+
+describe('User Logout All', () => {
+    it('should log the user out of all the devices', (done) => {
+      let utokens = user.tokens;
+      chai
+        .request(app)
+        .post('/users/logout')
+        .set('Authorization', `Bearer ${utokens[0].token}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message');
+          done();
+        });
+    });
+  });
+  
+describe('User Update', () => {
+    it('should update user details', (done) => {
+      let utokens = user.tokens;
+        chai
+            .request(app)
+            .put('/users/update/self')
+            .set('Authorization', `Bearer ${utokens[0].token}`)
+            .send({
+                name: "Johnnie Doe"
+            })
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('message');
+                res.body.should.have.property('data');
+                done();
+            })
+    })
+})
+
+describe('User Delete', () => {
+    it('should delete current user', (done) => {
+        let utokens = user.tokens;
+        chai
+            .request(app)
+            .delete('/users/delete/self')
+            .set('Authorization', `Bearer ${utokens[0].token}`)
+            .end((err,res) => {
+                res.should.have.status(200);
+                res.body.should.have.property('message')
+                done();
+            })
+    })
+})
+
